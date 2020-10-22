aaa678e06e47d6fa10ac1e88d45339698a2329d3
Rafał Plinzner <rplinzner@gmail.com>
2020-10-22 21:26:34 +0200

    [Add] changing theme according to system settings

--- movies-grader/src/contexts/ThemeContext.tsx
+++ movies-grader/src/contexts/ThemeContext.tsx
@@ -20,12 +20,24 @@ interface Props {
 
 export const ThemeProvider = (props: Props) => {
   const { children } = props;
-  const [theme, setTheme] = useState<themeMode>(themeMode.dark);
+  const [theme, setTheme] = useState<themeMode>(
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? themeMode.dark
+      : themeMode.light
+  );
   const changeTheme = () => {
     setTheme((prev) =>
       prev === themeMode.dark ? themeMode.light : themeMode.dark
     );
   };
+
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", (e) => {
+      const newColorScheme = e.matches ? themeMode.dark : themeMode.light;
+      setTheme(newColorScheme);
+    });
+
   return (
     <ThemeContext.Provider value={{ theme, changeTheme }}>
       {children && children(theme)}
